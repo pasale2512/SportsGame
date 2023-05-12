982f577a9c753841d757b30b9e86f3f407e39898
simon <stplosim@bx.fallmerayer.it>
2023-05-12 13:07:15 +0200

    store

--- app/src/main/java/com/example/sportsgame/Store.java
+++ app/src/main/java/com/example/sportsgame/Store.java
@@ -2,86 +2,112 @@ package com.example.sportsgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.view.ViewGroup.LayoutParams;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Store extends AppCompatActivity {
+    private List<Integer> purchasedImages1; // Liste für gekaufte Bilder
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_store);
 
-
-        //Big Pic
-        ImageView imageView00 = findViewById(R.id.imageView00);
-        imageView00.setImageResource(R.drawable.shopitem);
-
-        //Player
-        ImageView imageView10 = findViewById(R.id.imageView10);
-        imageView10.setImageResource(R.drawable.shop_black_player);
-        ImageView imageView11 = findViewById(R.id.imageView11);
-        imageView11.setImageResource(R.drawable.shop_brown_player);
-        ImageView imageView12 = findViewById(R.id.imageView12);
-        imageView12.setImageResource(R.drawable.shop_normal_player);
-        ImageView imageView13 = findViewById(R.id.imageView13);
-        imageView13.setImageResource(R.drawable.shop_white_player);
-        ImageView imageView14 = findViewById(R.id.imageView14);
-        imageView14.setImageResource(R.drawable.shop_yellow_player);
-        ImageView imageView15 = findViewById(R.id.imageView15);
-        imageView15.setImageResource(R.drawable.shop_tomato_player);
-
-
-        //Shirts
-        ImageView imageView20 = findViewById(R.id.imageView20);
-        imageView20.setImageResource(R.drawable.shop_blue_shirt);
-        ImageView imageView21 = findViewById(R.id.imageView21);
-        imageView21.setImageResource(R.drawable.shop_green_shirt);
-        ImageView imageView22 = findViewById(R.id.imageView22);
-        imageView22.setImageResource(R.drawable.shop_grey_shirt);
-        ImageView imageView23 = findViewById(R.id.imageView23);
-        imageView23.setImageResource(R.drawable.shop_white_shirt);
-        ImageView imageView24 = findViewById(R.id.imageView24);
-        imageView24.setImageResource(R.drawable.shop_yellow_shirt);
-        ImageView imageView25 = findViewById(R.id.imageView25);
-        imageView25.setImageResource(R.drawable.shop_red_shirt);
-
-
-        //Shorts
-        ImageView imageView30 = findViewById(R.id.imageView30);
-        imageView30.setImageResource(R.drawable.shop_blue_shorts);
-        ImageView imageView31 = findViewById(R.id.imageView31);
-        imageView31.setImageResource(R.drawable.shop_green_shorts);
-        ImageView imageView32 = findViewById(R.id.imageView32);
-        imageView32.setImageResource(R.drawable.shop_grey_shorts);
-        ImageView imageView33 = findViewById(R.id.imageView33);
-        imageView33.setImageResource(R.drawable.shop_white_shorts);
-        ImageView imageView34 = findViewById(R.id.imageView34);
-        imageView34.setImageResource(R.drawable.shop_yellow_shorts);
-        ImageView imageView35 = findViewById(R.id.imageView35);
-        imageView35.setImageResource(R.drawable.shop_red_shorts);
+        purchasedImages1 = loadPurchasedImages(); // Spielstand laden
 
         // Erstelle ScrollView
         ScrollView scrollView = new ScrollView(this);
         scrollView.setLayoutParams(new ScrollView.LayoutParams(ScrollView.LayoutParams.MATCH_PARENT, ScrollView.LayoutParams.MATCH_PARENT));
 
+        // Erstelle LinearLayout als Container für ScrollView
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        linearLayout.setGravity(Gravity.END); // Verschiebe den Inhalt nach rechts
+
         // Erstelle GridLayout
         GridLayout gridLayout = new GridLayout(this);
         gridLayout.setColumnCount(6);
-        gridLayout.setRowCount(6);
+        gridLayout.setRowCount(7);
 
         // Füge ImageViews hinzu
-        for (int i = 1; i <= 36; i++) {
+        for (int i = 1; i <= 41; i++) {
             ImageView imageView = new ImageView(this);
             imageView.setImageResource(getResources().getIdentifier("image" + i, "drawable", getPackageName()));
+            imageView.setLayoutParams(new LayoutParams(200, 200));
+            imageView.setTag(i); // Setze den Tag, um das Bild zu identifizieren
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int imageId = (int) v.getTag();
+                    ImageView clickedImageView = (ImageView) v;
+                    clickedImageView.setImageResource(R.drawable.gekauft); // Setze das Bild "gekauft"
+                    clickedImageView.setClickable(false); // Deaktiviere den Klicklistener
+
+                    purchasedImages1.add(imageId); // Bild zur Liste der gekauften Bilder hinzufügen
+                    savePurchasedImages(purchasedImages1); // Spielstand speichern
+                }
+            });
+
+            if (purchasedImages1.contains(i)) { // Prüfe, ob das Bild bereits gekauft wurde
+                imageView.setImageResource(R.drawable.gekauft);
+                imageView.setClickable(false);
+            }
+
             gridLayout.addView(imageView);
         }
 
-        // Füge GridLayout zu ScrollView hinzu
-        scrollView.addView(gridLayout);
+        // Füge GridLayout zu LinearLayout hinzu
+        linearLayout.addView(gridLayout);
+
+        // Füge LinearLayout zur ScrollView hinzu
+        scrollView.addView(linearLayout);
 
         // Setze ContentView auf ScrollView
         setContentView(scrollView);
+    }
+
+    // Spielstand laden
+    private List<Integer> loadPurchasedImages() {
+        List<Integer> purchasedImages1 = new ArrayList<>();
+        try {
+            File file = new File(getFilesDir(), "purchased_images1.txt");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null) {
+                int imageId = Integer.parseInt(line);
+                purchasedImages1.add(imageId);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return purchasedImages1;
+    }
+    // Spielstand speichern
+    private void savePurchasedImages(List<Integer> purchasedImages1) {
+        try {
+            File file = new File(getFilesDir(), "purchased_images1.txt");
+            FileOutputStream fos = new FileOutputStream(file);
+            for (Integer imageId : purchasedImages1) {
+                String line = imageId + "\n";
+                fos.write(line.getBytes());
+            }
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
